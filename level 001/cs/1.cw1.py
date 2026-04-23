8d743009a92a064d877f22a2b5c7638e3622e879
zangebissakhe-web <zangebissakhe@gmail.com>
2026-04-23 21:17:49 +0400

    home work

---
+++ level 001/cs/1.cw1.py
@@ -0,0 +1,6 @@
+#ტერმინალი არის ადგილი სადაც ჩვენი კოდის შედეგი გამოდის
+print("inga kalandadze")
+print("zaal zaalishvili")
+print("aleqsandre zaalishvili")
+print("demetre zalishvili")
+#printi-ის მეშვეობით შეგვიძლია კოდი გამოვიყვანოთ ტერმინალი
---
+++ level 001/hw/hw.2.py
@@ -0,0 +1,14 @@
+#კომენტარს ჩვენ ვიყენებთ რომ ჩავინიშნოთ,ავხსნათ და ასეშემდეგ   1 დავალება
+
+#დავალება 2
+print('xinklali')
+print('cixirtma')
+print('mwvadi')
+print('sushi')
+print('carbonara')
+#3 დავალება
+name = 'deme'
+age = 13
+surname = 'zaalishvili'
+# 4 დავალება0
+print(name,surname,age)
