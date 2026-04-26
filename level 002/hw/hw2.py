edfbdb66976001b9100f3b881a7d986876d87561
zangebissakhe-web <zangebissakhe@gmail.com>
2026-04-26 15:35:04 +0400

    hw

---
+++ level 002/hw/hw2.py
@@ -0,0 +1,49 @@
+#დავალებ 1
+
+#string
+print('tagvi')
+print('mwvadi')
+#int
+print(10)
+print(120)
+#float
+print('1.20') 
+print('30.12345')
+#boolean
+print(True)
+print(False)
+
+#დავალება 2
+#კონკატინაცია არის სტრინგის დამატება სტრინგთან
+name ='maxo'
+hi = 'gamarjoba '
+print(hi + name)
+#დავალება 3
+
+#debuging-ს ვიყენებთ კოდის შესასწორებლად ერორების განსახილველად ან კოდი რო ავხსნათ
+
+#დავალება 4
+
+name = 'deme '
+print('hello my name is ' + name)
+#დავალება 5
+num1 = 10
+num2 = 67
+num3 = 21
+print(num1 * num2 * num3)
+#დავალება 6
+lenght = 40
+width = 30
+print((lenght + width)* 2)
+#დავალება 7
+print(2+2)
+print(1+1)
+
+print(12323423-313232)
+print(10 -2)
+
+print(12*73)
+print(98* 67)
+
+print(100 / 100)
+print(70 / 3)
